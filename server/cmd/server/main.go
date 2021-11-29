0afb8698254d789822469cc7b461837aef81b5db
= <mypochtav@gmail.com>
2021-11-29 14:48:43 +0200

    fix conflict with packages

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,10 +1,14 @@
-package server
+package main
 
-import "log"
+import (
+	"github.com/vlad1m1r0v/APZ-3/server/pkg/handler"
+	"log"
+)
 
 func main () {
+	handler := new(handler.Handler)
 	server := new(Server)
-	if err := server.Run("8000"); err != nil {
+	if err := server.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
